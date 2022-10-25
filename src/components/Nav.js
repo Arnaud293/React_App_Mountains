6e1189877276e42f6919ecb064055a27175f59f3
Arnaud293 <arnaudlt293@gmail.com>
2022-10-25 11:22:12 +0200

    add nav as header sub-component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul className="nav-menu">
+                <a href=""><li>This is Massively</li></a>
+                <a href=""><li>Generic page</li></a>
+                <a href=""><li>Element Reference</li></a>
+            </ul>
+            <ul className="nav-icon">
+                <a href=""><li><i className="fa-brands fa-twitter"></i></li></a>
+                <a href=""><li><i className="fa-brands fa-facebook-f"></i></li></a>
+                <a href=""><li><i className="fa-brands fa-instagram"></i></li></a>
+                <a href=""><li><i className="fa-brands fa-github"></i></li></a>
+            </ul>
+        </nav>
     );
 };
 
