f570c1c37cfc7567a0975b6f48747128cb561f60
Arnaud293 <arnaudlt293@gmail.com>
2022-10-25 11:21:48 +0200

    add base structure for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
+
 import React from 'react';
+import Nav from './Nav';
+
 
 const Header = () => {
     return (
         <div>
-            <h1>header</h1>
+            <header>
+        <div className="header-top">
+            <h1>La Montagne <br/> ça vous gagne</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elitm esse omnis, aliquid a, ametdolor, hic itaque ullam assumenda fuga natus nesciunt officiis placeat!</p>
+            <br/>
+            <br/>
+            <br/>
+            <i class="fa-solid fa-circle-arrow-down"></i>  
+        </div>
+        <Nav />
+    </header>
         </div>
     );
 };
