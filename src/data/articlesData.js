c6b9e9b091178131cc90d00e955d9212bc1d0e17
Arnaud293 <arnaudlt293@gmail.com>
2022-10-25 16:30:00 +0200

    add articles data in a separate folder

---
+++ src/data/articlesData.js
@@ -0,0 +1,25 @@
+export const articlesData = [
+    {
+        id : 1,
+        title : "Lorem 1",
+        date : 'October 25, 2022',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, fuga eaque consequuntur adipisci libero sequi voluptate voluptatibus id inventore soluta vel exercitationem laborum quia dolorum aspernatur aperiam, natus distinctio doloremque tempora animi quibusdam enim? Autem atque expedita sit numquam! Minus aut sint laboriosam, dolores, officiis architecto et suscipit vel corrupti tempora consectetur eaque impedit quo corporis alias veniam dicta! Expedita minima asperiores dolorum iste minus, blanditiis libero quisquam, ut deserunt eius vero delectus voluptate sit, magnam sapiente? Voluptate quisquam, temporibus blanditiis eos rerum, iste, dolorum tenetur aperiam sit vel nisi cupiditate consequatur odit ex! Incidunt, nam officia? Autem cumque, esse atque deleniti quam recusandae dicta repellat, neque quidem id quod quaerat iste modi facilis voluptas quasi veritatis ipsa dolores perferendis consectetur, ut quas ad quos. Enim quia excepturi, nihil cum tenetur distinctio illum quaerat reprehenderit repellat optio dolores exercitationem consectetur, maxime necessitatibus ea unde porro, quas non! Tenetur omnis cupiditate accusantium a pariatur deleniti neque? Debitis, ipsa? Necessitatibus est fugit magnam soluta nulla, sit dolorum error omnis beatae non officiis, laborum ducimus, autem reprehenderit perferendis vitae distinctio nobis enim. Qui illum natus repellendus asperiores labore vel quia voluptas error impedit, minima culpa ducimus, sapiente rem architecto ipsum, aliquam dolores. Alias.',
+        img :'./assets/images/pic01.jpg'
+    },
+
+    {
+        id : 2,
+        title : "Lorem 2",
+        date : 'October 24, 2022',
+        text: 'Porem ipsum dolor sit amet consectetur adipisicing elit. Est, fuga eaque consequuntur adipisci libero sequi voluptate voluptatibus id inventore soluta vel exercitationem laborum quia dolorum aspernatur aperiam, natus distinctio doloremque tempora animi quibusdam enim? Autem atque expedita sit numquam! Minus aut sint laboriosam, dolores, officiis architecto et suscipit vel corrupti tempora consectetur eaque impedit quo corporis alias veniam dicta! Expedita minima asperiores dolorum iste minus, blanditiis libero quisquam, ut deserunt eius vero delectus voluptate sit, magnam sapiente? Voluptate quisquam, temporibus blanditiis eos rerum, iste, dolorum tenetur aperiam sit vel nisi cupiditate consequatur odit ex! Incidunt, nam officia? Autem cumque, esse atque deleniti quam recusandae dicta repellat, neque quidem id quod quaerat iste modi facilis voluptas quasi veritatis ipsa dolores perferendis consectetur, ut quas ad quos. Enim quia excepturi, nihil cum tenetur distinctio illum quaerat reprehenderit repellat optio dolores exercitationem consectetur, maxime necessitatibus ea unde porro, quas non! Tenetur omnis cupiditate accusantium a pariatur deleniti neque? Debitis, ipsa? Necessitatibus est fugit magnam soluta nulla, sit dolorum error omnis beatae non officiis, laborum ducimus, autem reprehenderit perferendis vitae distinctio nobis enim. Qui illum natus repellendus asperiores labore vel quia voluptas error impedit, minima culpa ducimus, sapiente rem architecto ipsum, aliquam dolores. Alias.',
+        img :'./assets/images/pic02.jpg'
+    },
+
+    {
+        id : 3,
+        title : "Lorem 3",
+        date : 'Octobre 23, 2022',
+        text: 'Sorem ipsum dolor sit amet consectetur adipisicing elit. Est, fuga eaque consequuntur adipisci libero sequi voluptate voluptatibus id inventore soluta vel exercitationem laborum quia dolorum aspernatur aperiam, natus distinctio doloremque tempora animi quibusdam enim? Autem atque expedita sit numquam! Minus aut sint laboriosam, dolores, officiis architecto et suscipit vel corrupti tempora consectetur eaque impedit quo corporis alias veniam dicta! Expedita minima asperiores dolorum iste minus, blanditiis libero quisquam, ut deserunt eius vero delectus voluptate sit, magnam sapiente? Voluptate quisquam, temporibus blanditiis eos rerum, iste, dolorum tenetur aperiam sit vel nisi cupiditate consequatur odit ex! Incidunt, nam officia? Autem cumque, esse atque deleniti quam recusandae dicta repellat, neque quidem id quod quaerat iste modi facilis voluptas quasi veritatis ipsa dolores perferendis consectetur, ut quas ad quos. Enim quia excepturi, nihil cum tenetur distinctio illum quaerat reprehenderit repellat optio dolores exercitationem consectetur, maxime necessitatibus ea unde porro, quas non! Tenetur omnis cupiditate accusantium a pariatur deleniti neque? Debitis, ipsa? Necessitatibus est fugit magnam soluta nulla, sit dolorum error omnis beatae non officiis, laborum ducimus, autem reprehenderit perferendis vitae distinctio nobis enim. Qui illum natus repellendus asperiores labore vel quia voluptas error impedit, minima culpa ducimus, sapiente rem architecto ipsum, aliquam dolores. Alias.',
+        img :'./assets/images/pic03.jpg'
+    },
+];
